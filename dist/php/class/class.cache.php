29c850644950b2461c40ba9618c325ce31267195
thibobbs <thibaut.renard@hetic.net>
2017-03-29 19:17:10 +0200

    cache update

--- dist/php/class/class.cache.php
+++ dist/php/class/class.cache.php
@@ -4,6 +4,7 @@ class Cache {
   
   public $dirname;
   public $duration;
+  public $buffer;
   
   public function __construct($dirname, $duration) {
     $this->dirname = $dirname;
@@ -16,10 +17,60 @@ class Cache {
   
   public function read ($filename) {
     $file = $this->dirname.'/'.$filename;
+    if (!file_exists($file)) {
+      return false;
+    }
     $lifetime = (time() - filemtime($file))/60;
     if ($lifetime > $this->duration) {
       return false;
     }
     return file_get_contents($file);
+  }
+  
+  public function delete($filename) {
+    $file = $this->dirname.'/'.$filename;
+    if (file_exists($file)) {
+      unlink($file);
+    }
+  }
+  
+  public function clear() {
+    $files = glob($this->dirname.'/*');
+    foreach ($files as $file) {
+      unlink($file);
+    }
+  }
+  
+  public function inc($file) {
+    $filename = basename($file);
+    if ($content = $this->read($filename)) {
+      echo $content;
+      return;
+    }
+    ob_start();
+    require $file;
+    $content = ob_get_clean();
+    $this->write($filename, $content);
+    echo $content;
+    return;
+  }
+  
+  public function start($cachename) {
+    if($content = $this->read($cachename)) {
+      echo $content;
+      $this->buffer = false;
+      return;
+    }
+    ob_start();
+    $this->buffer = $cachename;
+  }
+  
+  public function end() {
+    if (!$this->buffer) {
+      return false;
+    }
+    $content = ob_get_clean();
+    echo $content;
+    $this->write($this->buffer, $content);
   }
 }
